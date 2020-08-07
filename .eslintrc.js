7474269acd48cf0291ee6d46edb4ba52c6631059
Bridget Asser <bridgetasser@gmail.com>
2020-08-06 23:27:53 -0400

    settin up shop

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+    env: {
+        commonjs: true,
+        node: true,
+        es2020: true,
+    },
+    extends: "eslint:recommended",
+    parserOptions: {
+        ecmaVersion: 11,
+    },
+    rules: {},
+};
